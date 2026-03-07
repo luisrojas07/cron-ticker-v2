cca1f005a541a72a5daad48e31cafb5004db2655
luisrojas07 <luisrojasm7@gmail.com>
2026-03-07 02:02:01 -0400

    adding first workflow v4 provocamo falla

--- tests/sync.test.js
+++ tests/sync.test.js
@@ -3,7 +3,7 @@ const { syncTest } = require('../tasks/sync');
 describe('Pruebas de Sync Test', () => {
 
     test('debe de ejecutar el proceso 2 veces', () => {
-        syncTest();
+        //syncTest();
         const times = syncTest();
         console.log('Se llamo ', times);
 
